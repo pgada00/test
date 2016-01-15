4c88d49dc58eb212c47f01efd45b021ea0ebc636
benofben <ben.lackey@outlook.com>
2016-01-14 18:22:44 -0800

    don't use rg name since we're going to be cross region

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 RESOURCE_GROUP=$1
 
-azure group create $1 "US West"
+azure group create $1 "West US"
 azure group deployment create -f templates/mainTemplate.json -e parameters/prod.parameters.json $RESOURCE_GROUP dse
 
