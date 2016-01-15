311964dcd466b3d4c808d2e85b36aff91720cb69
benofben <ben.lackey@outlook.com>
2016-01-15 15:05:03 -0800

    shell script curls now

--- extensions/opscenter.sh
+++ extensions/opscenter.sh
@@ -15,3 +15,11 @@ apt-get -y install opscenter=5.2.3
 
 echo "Starting OpsCenter"
 sudo service opscenterd start
+
+wget https://raw.githubusercontent.com/J4U-Nimbus/EMJU_DataStaxARM/master/extensions/opscenter.py
+
+echo "Generating a provision.json file"
+python opscenter.py
+
+echo "Provisioning a new cluster using provision.json"
+curl --insecure -H "Accept: application/json" -X POST http://127.0.0.1:8888/provision -d @provision.json
