a5bcd7666cbd0332a4378b2c7fc942b0e37659b4
benofben <ben.lackey@outlook.com>
2016-01-16 13:01:30 -0800

    delete inexplicable variable.  This needs to be tested!!!

--- deploy.ps1
+++ deploy.ps1
@@ -4,18 +4,16 @@ $tenantID = "b7f604a0-00a9-4188-9248-42f3a5aac2e9"
 $subscriptionID = "dc2a4b35-5221-412c-bca8-cb90aef6fbfb"
 $subscriptionName = "EMJU"
 $ResourceGroupLocation = "westus"
- 
-$DSETemplate = "templates\mainTemplate.json"
- 
+
 $subscriptionArgs = @()
 $subscriptionArgs += ("-ResourceGroupLocation", $ResourceGroupLocation)
 $subscriptionArgs += ("-SubscriptionID",$subscriptionID)
 $subscriptionArgs += ("-SubscriptionName",$subscriptionName)
 $subscriptionArgs += ("-TenantID",$tenantID)
- 
+
 $invokeArgs = @()
 $invokeArgs += ("-ResourceGroupName","test")
-$invokeArgs += ("-TemplateFile", $DSETemplate)
+$invokeArgs += ("-TemplateFile", "templates\mainTemplate.json")
 $invokeArgs += ("-TemplateParametersFile","parameters\prod.parameters.json")
 $invokeArgs += $subscriptionArgs
 
