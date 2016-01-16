c8bce65e444a2a7c27625a9c677e8cd57b722830
benofben <ben.lackey@outlook.com>
2016-01-15 18:35:01 -0800

    deploy script from Gupreet

---
+++ deploy.bat
@@ -0,0 +1,23 @@
+# Login-AzureRmAccount
+ 
+$tenantID = "b7f604a0-00a9-4188-9248-42f3a5aac2e9"
+$subscriptionID = "dc2a4b35-5221-412c-bca8-cb90aef6fbfb"
+$subscriptionName = "EMJU"
+$ResourceGroupLocation = "westus"
+ 
+$DSETemplate = "..\templates\mainTemplate.json"
+ 
+$subscriptionArgs = @()
+$subscriptionArgs += ("-ResourceGroupLocation", $ResourceGroupLocation)
+$subscriptionArgs += ("-SubscriptionID",$subscriptionID)
+$subscriptionArgs += ("-SubscriptionName",$subscriptionName)
+$subscriptionArgs += ("-TenantID",$tenantID)
+ 
+ 
+##Datastax
+$invokeArgs = @()
+$invokeArgs += ("-ResourceGroupName","TESTAGAIN")
+$invokeArgs += ("-TemplateFile", $DSETemplate)
+$invokeArgs += ("-TemplateParametersFile","..\parameters\prod.parameters.json")
+$invokeArgs += $subscriptionArgs
+
