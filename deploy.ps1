28115c3ddcd39cfce21ec90b81dec3e4c923659e
benofben <ben.lackey@outlook.com>
2016-01-16 12:59:24 -0800

    clean up powershell.  this script has not been tested!!!

--- deploy.ps1
+++ deploy.ps1
@@ -1,11 +1,11 @@
-# Login-AzureRmAccount
+Login-AzureRmAccount
  
 $tenantID = "b7f604a0-00a9-4188-9248-42f3a5aac2e9"
 $subscriptionID = "dc2a4b35-5221-412c-bca8-cb90aef6fbfb"
 $subscriptionName = "EMJU"
 $ResourceGroupLocation = "westus"
  
-$DSETemplate = "..\templates\mainTemplate.json"
+$DSETemplate = "templates\mainTemplate.json"
  
 $subscriptionArgs = @()
 $subscriptionArgs += ("-ResourceGroupLocation", $ResourceGroupLocation)
@@ -13,11 +13,9 @@ $subscriptionArgs += ("-SubscriptionID",$subscriptionID)
 $subscriptionArgs += ("-SubscriptionName",$subscriptionName)
 $subscriptionArgs += ("-TenantID",$tenantID)
  
- 
-##Datastax
 $invokeArgs = @()
-$invokeArgs += ("-ResourceGroupName","TESTAGAIN")
+$invokeArgs += ("-ResourceGroupName","test")
 $invokeArgs += ("-TemplateFile", $DSETemplate)
-$invokeArgs += ("-TemplateParametersFile","..\parameters\prod.parameters.json")
+$invokeArgs += ("-TemplateParametersFile","parameters\prod.parameters.json")
 $invokeArgs += $subscriptionArgs
 
