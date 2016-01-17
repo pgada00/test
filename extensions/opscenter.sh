b6ae173c2b783dc3e50c8412e860a5a29af513fe
benofben <ben.lackey@outlook.com>
2016-01-17 00:39:45 -0800

    use azul
    Having lots of problems installing the Oracle JDK.  Going back to Azul.

--- extensions/opscenter.sh
+++ extensions/opscenter.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-echo "Installing Java"
-add-apt-repository -y ppa:webupd8team/java
+echo "Installing Azul Zulu JDK"
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
+apt-add-repository -y "deb http://repos.azulsystems.com/ubuntu stable main"
 apt-get -y update
-echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-apt-get -y install oracle-java8-installer
+apt-get -y install zulu-8
 
 echo "Installing OpsCenter"
 echo "deb http://debian.datastax.com/community stable main" | sudo tee -a /etc/apt/sources.list.d/datastax.community.list
