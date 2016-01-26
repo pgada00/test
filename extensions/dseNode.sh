991d3650d0a351bbd3b8680e3bf05d99ea81cb97
ClaytonPeddy <ECUnderground@hotmail.com>
2016-01-26 13:41:05 -0800

    Added Parameters to DSE script

--- extensions/dseNode.sh
+++ extensions/dseNode.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+DATACENTERNAME=$1
+SEEDNODEADDRESS=$2
+OPSCENTERADDRESS=$3
+
 echo "Installing Azul Zulu JDK"
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
 apt-add-repository -y "deb http://repos.azulsystems.com/ubuntu stable main"
