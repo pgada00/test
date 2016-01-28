9cd24f06fee70f69e3f56057523f432a66d2b992
root <root@prwestoltpvm0.nsufyddt3txujgpg2cd0sbjiof.dx.internal.cloudapp.net>
2016-01-28 19:29:53 +0000

    Updated address.yaml edits

--- extensions/dseNode.sh
+++ extensions/dseNode.sh
@@ -241,7 +241,7 @@ cat cassandra-rackdc.properties \
 #
 cd "$agent_conf_dir"
 stomp_interface=$opscenter_public_ip
-local_interface=$node_public_ip
+local_interface=$node_private_ip
 agent_rpc_broadcast_address=$node_public_ip
 agent_rpc_interface=$node_private_ip
 use_ssl=0
@@ -250,19 +250,19 @@ async_pool_size=10
 async_queue_size=800000
 max_reconnect_time=60000
 cat address.yaml \
-| sed -e "s:.*\(stomp_interface\:\).*:stomp_interface\: $opscenter_public_ip:" \
-| sed -e "s:.*\(local_interface\:\).*:local_interface\: $node_public_ip:" \
-| sed -e "s:.*\(agent_rpc_broadcast_address\:\).*:agent_rpc_broadcast_address\: $node_public_ip:" \
-| sed -e "s:.*\(agent_rpc_interface\:\).*:agent_rpc_interface\: $node_private_ip:" \
+| sed -e "s:.*\(stomp_interface\:\).*:stomp_interface\: $stomp_interface:" \
+| sed -e "s:.*\(local_interface\:\).*:local_interface\: $local_interface:" \
+| sed -e "s:.*\(agent_rpc_broadcast_address\:\).*:agent_rpc_broadcast_address\: $agent_rpc_broadcast_address:" \
+| sed -e "s:.*\(agent_rpc_interface\:\).*:agent_rpc_interface\: $agent_rpc_interface:" \
 | sed -e "s:.*\(use_ssl\:\).*:use_ssl\: $use_ssl:" \
 | sed -e "s:.*\(thrift_max_conns\:\).*:thrift_max_conns\: $thrift_max_conns:" \
 | sed -e "s:.*\(async_pool_size\:\).*:async_pool_size\: $async_pool_size:" \
 | sed -e "s:.*\(max_reconnect_time\:\).*:max_reconnect_time\: $max_reconnect_time:" \
 > address.yaml.new
-if [ "x$(grep stomp_interface address.yaml)" == x ]; then echo "stomp_interface: $opscenter_public_ip" >> address.yaml.new; fi
-if [ "x$(grep local_interface address.yaml)" == x ]; then echo "local_interface: $node_public_ip" >> address.yaml.new; fi
-if [ "x$(grep agent_rpc_broadcast_address address.yaml)" == x ]; then echo "agent_rpc_broadcast_address: $node_public_ip" >> address.yaml.new; fi
-if [ "x$(grep agent_rpc_interface address.yaml)" == x ]; then echo "agent_rpc_interface: $node_private_ip" >> address.yaml.new; fi
+if [ "x$(grep stomp_interface address.yaml)" == x ]; then echo "stomp_interface: $stomp_interface" >> address.yaml.new; fi
+if [ "x$(grep local_interface address.yaml)" == x ]; then echo "local_interface: $local_interface" >> address.yaml.new; fi
+if [ "x$(grep agent_rpc_broadcast_address address.yaml)" == x ]; then echo "agent_rpc_broadcast_address: $agent_rpc_broadcast_address" >> address.yaml.new; fi
+if [ "x$(grep agent_rpc_interface address.yaml)" == x ]; then echo "agent_rpc_interface: $agent_rpc_interface" >> address.yaml.new; fi
 if [ "x$(grep use_ssl address.yaml)" == x ]; then echo "use_ssl: $use_ssl" >> address.yaml.new; fi
 if [ "x$(grep thrift_max_conns address.yaml)" == x ]; then echo "thrift_max_conns: $thrift_max_conns" >> address.yaml.new; fi
 if [ "x$(grep async_pool_size address.yaml)" == x ]; then echo "async_pool_size: $async_pool_size" >> address.yaml.new; fi
