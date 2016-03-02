f74e4f5d7a003d4686603a8b0b7d3372bcb9f646
GS <gurpreet.singh@safeway.com>
2016-03-01 19:29:16 -0800

    updated the new changes

--- test/deploy.ps1
+++ test/deploy.ps1
@@ -3,7 +3,7 @@ Login-AzureRmAccount
 $tenantID = "b7f604a0-00a9-4188-9248-42f3a5aac2e9"
 $subscriptionID = "ec4b8e6e-6a27-4430-a1ac-3ded010cb563"
 $ResourceGroupLocation = "westus"
-$ResourceGroupName = "EMJU-AZTESTBKPCOPY-DSE"
+$ResourceGroupName = "EMJU-AZTESTBKP-DSE"
 $DeploymentName = "TestDSEDeployment"
 
 $DSETemplate = "templates\mainTemplate.json"
